842bdd6a4dd16bd0c16e036ebddaf7e483be038b
Recep ALICI <alicirecepp@gmail.com>
2024-05-21 19:41:02 +0200

    first commit

---
+++ src/test/java/helperDB/JDBC_Cons.java
@@ -0,0 +1,11 @@
+package helperDB;
+
+import com.github.javafaker.Faker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBC_Cons {
+    static Faker faker=new Faker();
+
+}
---
+++ src/test/java/manage/Manage.java
@@ -0,0 +1,10 @@
+package manage;
+
+public class Manage {
+
+
+    //private String Query01 = "SELECT user_id FROM deposits Where amount > 100 And amount <500;";
+    /**
+     * US_01 Categories tablosunda 'slug = fashion' olan bir categorie'nin name değerini dogrulayınız.
+     */
+}
