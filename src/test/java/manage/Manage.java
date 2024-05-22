f769cb155e07f59cba34382ffb86badd14113be7
Recep ALICI <alicirecepp@gmail.com>
2024-05-22 20:38:22 +0200

    merhaba123456

--- src/test/java/manage/Manage.java
+++ src/test/java/manage/Manage.java
@@ -5,7 +5,7 @@ public class Manage {
 
     //private String Query01 = "SELECT user_id FROM deposits Where amount > 100 And amount <500;";
     /**
-	 * bos birakilamaz mi?
+	 * bos birakilamaz mi? dediniz
      * US_01 Categories tablosunda 'slug = fashion' olan bir categorie'nin name değerini dogrulayınız.
      */
 }
